592ec1cc60df79ddd7ce7be95be5f9570244958d
Qiuqun Wang <qiuqunwang12@gmail.com>
2018-10-27 19:35:01 -0400

    modified script.js to use document Object

--- QueensHackAttack/app/static/script.js
+++ QueensHackAttack/app/static/script.js
@@ -1,39 +1,37 @@
-$(document).ready(function() {
+document.getElementById("createEvent_button").click(function() {
+    var x = document.getElementsByClassName("createEvent");
+    var y = document.getElementsByClassName("viewEvent");
+    var z = document.getElementsByClassName("setup");
 
-    $('#createEvent_button').click(function() {
-        var x = document.getElementsByClassName("createEvent");
-        var y = document.getElementsByClassName("viewEvent");
-        var z = document.getElementsByClassName("setup");
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
 
-        x.style.display = "block";
-        y.style.display = "none";
-        z.style.display = "none";
+});
 
-    });
 
-    $('#viewEvent_button').click(function() {
-        var x = document.getElementsByClassName("createEvent");
-        var y = document.getElementsByClassName("viewEvent");
-        var z = document.getElementsByClassName("setup");
+document.getElementById("viewEvent_button").click(function() {
+    var x = document.getElementsByClassName("createEvent");
+    var y = document.getElementsByClassName("viewEvent");
+    var z = document.getElementsByClassName("setup");
 
-        x.style.display = "none";
-        y.style.display = "block";
-        z.style.display = "none";
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "none";
 
-    });
+});
 
-    $('#setup_button').click(function() {
-        var x = document.getElementsByClassName("createEvent");
-        var y = document.getElementsByClassName("viewEvent");
-        var z = document.getElementsByClassName("setup");
+document.getElementById("setup_button").click(function() {
+    var x = document.getElementsByClassName("createEvent");
+    var y = document.getElementsByClassName("viewEvent");
+    var z = document.getElementsByClassName("setup");
 
-        x.style.display = "none";
-        y.style.display = "none";
-        z.style.display = "block";
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "block";
 
-    });
-
+});
 
 
 
-})
+
